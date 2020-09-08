06c284c03fc411bfc3bcda2f333b17d2b7b961b9
bassacool <bassacool@gmail.com>
2020-09-08 13:38:03 +0000

    first version

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get('/liste',(req,res)=>{
+    res.send([
+        {
+            id:1,
+            nom:"coulibaly"
+        },
+        {
+            id:2,
+            nom:"Kone"
+        }
+    ])
+});
+
+app.listen(3000,()=>{
+    console.log("good")
+})
